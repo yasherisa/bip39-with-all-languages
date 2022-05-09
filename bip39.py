9c60e1acc03cfb792694859a5d9da5620663bd58
yasherisa <87241499+yasherisa@users.noreply.github.com>
2022-05-09 14:05:25 +0300

    Add files via upload

---
+++ bip39.py
@@ -0,0 +1,18 @@
+import chinese_simplified
+import chinese_traditional
+import english
+import french
+import italian
+import japanese
+import korean
+import spanish
+bip39_all_language = [
+    chinese_simplified.bip39,
+    chinese_traditional.bip39,
+    english.bip39,
+    french.bip39,
+    italian.bip39,
+    japanese.bip39,
+    korean.bip39,
+    spanish.bip39
+]
